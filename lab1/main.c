167c46d816e8417e82f00d2db75a87d19723d2bf
Demix361 <49446810+Demix361@users.noreply.github.com>
2019-11-20 18:42:43 +0300

    lab1

---
+++ lab1/main.c
@@ -0,0 +1,26 @@
+#include <LPC23xx.H> // Описание LPC23xx
+
+void delay(void) {
+	unsigned int i;
+	for (i=0;i<0xfffff;i++){}
+}
+
+int main(void) {
+	unsigned int n, k;
+ 	PINSEL3 = 0x00000000;
+ 	IODIR1 = 0x1C000000;
+ 	IOSET1 = 0x1C000000;
+ 	while (1) {
+ 		if (IOPIN1 & (1<<29))
+		{
+			IOSET1 = (1<<26);
+			IOCLR1 = (1<<27);
+			IOCLR1 = (1<<28);
+		}
+ 		else{
+			IOSET1 = (1<<27);
+			IOSET1 = (1<<28);
+			IOCLR1 = (1<<26);
+ 		}
+ 	}
+}
